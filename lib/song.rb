3480f7ed591975e7662bc62e98b24f5b1dc1cb10
Romano Livi <romano_livi@outlook.com>
2020-12-17 19:15:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,12 @@ class Song
       genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
+          genre_count[genre] += 1 
+        else 
+          genre_count[genre] = 1
+        end
+      genre_count
+    end
   end
   
 
