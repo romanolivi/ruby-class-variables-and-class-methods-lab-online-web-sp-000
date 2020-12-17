3e45794eb0b4c7d6b9552981625b818a7c9081c0
Romano Livi <romano_livi@outlook.com>
2020-12-17 18:26:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @genre = genre 
+    @artist = artist
+    @@count += 1 
+  end
+
+  def count
+    self.count
+  end
+
+end
