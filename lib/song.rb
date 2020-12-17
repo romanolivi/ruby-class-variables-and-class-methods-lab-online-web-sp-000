88d34369aeb747a16142570c3bd52eb348962076
Romano Livi <romano_livi@outlook.com>
2020-12-17 18:29:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,20 @@ class Song
   
   @@count = 0 
   
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name 
     @genre = genre 
     @artist = artist
     @@count += 1 
+    @@genres << genre
   end
 
   def self.count
     @@count
   end
+  
+  
 
 end
